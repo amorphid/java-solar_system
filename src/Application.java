2e9b85c9300976446d6afe20b4d4a2157adba621
Michael Pope <mpope.cr@gmail.com>
2015-01-09 09:47:59 -0500

    Add Planet

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,18 @@
 public class Application {
     public static void main(String[] args) {
-        // insert program here
+        Planet[] solarSystem = {
+                new Planet("Mercury"),
+                new Planet("Venus"),
+                new Planet("Earth"),
+                new Planet("Mars"),
+                new Planet("Jupiter"),
+                new Planet("Saturn"),
+                new Planet("Uranus"),
+                new Planet("Neptune")
+        };
+
+        for (Planet planet : solarSystem) {
+            System.out.println(planet.getName());
+        }
     }
 }
