2e044210dd38d8f29f6130d272ea46a953bf7c05
Michael Pope <mpope.cr@gmail.com>
2015-01-09 15:03:39 -0500

    Refactor

--- src/Planet.java
+++ src/Planet.java
@@ -2,8 +2,8 @@ public class Planet {
     public String name;
     public int orbitId;
 
-    public Planet (String name, int orbitID) {
+    public Planet (String name, int orbitId) {
         this.name    = name;
-        this.orbitId = orbitID;
+        this.orbitId = orbitId;
     }
 }
